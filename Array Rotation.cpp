d4c6c97ca1c4e5b4494e1c348fd8c815c2934f19
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-05-02 12:35:28 +0530

    s+=a;s=((s%MOD)+MOD)%MOD; not only s%MOD

---
+++ Array Rotation.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define ld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+#define MOD                 1000000007
+
+int gcd(int A, int B)
+{
+    if (B == 0)
+        return A;
+    else
+        return gcd(B, A % B);
+}
+
+int main()
+{
+    fastio;
+    lli n,s=0;
+    cin>>n;
+    lli a;
+    fn {cin>>a;s+=a;s=((s%MOD)+MOD)%MOD; }
+    lli q;
+    cin>>q;
+    lli x;
+    while(q--)
+    {
+        cin>>x;
+        s+=s;
+        s=((s%MOD)+MOD)%MOD;
+        cout<<s<<endl;
+    }
+    return 0;
+}
