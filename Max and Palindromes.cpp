3cd2ec90b195b50321f77645dd474a69c551737c
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-05-24 11:58:23 +0530

    IMP string cnpt, map cnpt ,L-2

---
+++ Max and Palindromes.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define sl                  set<lli>
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define ml                  map<<lli,lli>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define lld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+#define MOD                 1000000007
+
+int main()
+{
+    fastio;
+    string s;
+    cin>>s;
+    unordered_map<char,lli> m;
+    for(lli i=0; i<s.length(); i++)
+        m[s[i]]++;
+    lli l=m.size(), sum=0;
+    for(auto it:m)
+    {
+        if(it.second>2)
+            sum+=l;
+        else if(it.second == 2)
+            sum+=(l-1);
+    }
+    cout<<sum<<endl;
+    return 0;
+}
