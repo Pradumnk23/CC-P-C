7ca89c4e31533ff07cb5c62d3265d4bb7f08b1df
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-06-01 23:27:16 +0530

    if((k-x-y)>=0 && (k-x-y)%2==0)  ->yes;

---
+++ Tom And Jerry 1.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define sl                  set<lli>
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define ml                  map<<lli,lli>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define lld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+#define mod                 1000000007
+
+int main()
+{
+    fastio;
+    test()
+    {
+        lli a,b,c,d,k;
+        cin>>a>>b>>c>>d>>k;
+        lli y=abs(b-d);
+        lli x=abs(a-c);
+        if((k-x-y)>=0 && (k-x-y)%2==0)
+            yes;
+        else
+            no;
+    }
+    return 0;
+}
